3fa9b6d18649375b51b922a11e528b936818589b
Payam K <payamase@gmail.com>
2022-12-18 15:45:18 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# jwt-headers
-An R script to pull the JWT headers that are added by ALB
+# Get JWT Headers for R Shiny
+
+This chunk of R code can be used in a Shiny app to pull the JWT headers that are added by AWS' Application Load Balancer (ALB).
+
+This can be used for user authentication through IdP. More info and Python version of this code is provided by [AWS here](https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html), but I wanted to implement an R version to use in a Shiny app.
+
+It pulls the user's email from the request header (`x-amzn-oidc-data`) and stores it in a reactive object (`user_email`). If the Shiny app is running locally, it pulls the email from `GARGLE_EMAIL` environment variable (if it exists).
---
+++ get_jwt_header.R
@@ -0,0 +1,27 @@
+user_email <- shiny::reactive({
+  # Step 1: Get the key id from JWT headers
+  encoded_jwt <- session$request$HTTP_X_AMZN_OIDC_DATA
+  if (!is.null(encoded_jwt)) {
+    jwt_headers <- strsplit(encoded_jwt, ".", fixed = TRUE)
+    decoded_jwt_headers <- openssl::base64_decode(jwt_headers[[1]][1])
+    decoded_json <- jsonlite::fromJSON(rawToChar(decoded_jwt_headers))
+    kid <- decoded_json$kid
+    
+    # Step 2: Get the public key from regional endpoint
+    region_endpoint <- stringr::str_extract(decoded_json$signer, "us-\\w+-\\d")
+    url <- paste0("https://public-keys.auth.elb.", region_endpoint, ".amazonaws.com/", kid)
+    req <- httr::GET(url)
+    pub_key <- httr::content(req, as = "text", encoding = "UTF-8")
+    
+    # Step 3: Get the payload
+    payload <- jose::jwt_decode_sig(jwt = encoded_jwt, pubkey = pub_key)
+    
+    print(paste0("User email: ", tolower(payload$email)))
+    return(tolower(payload$email))
+  } else if (is.null(encoded_jwt) & interactive()) {
+    print(paste0("Local user email obtained from `GARGLE_EMAIL` (env var): ", Sys.getenv("GARGLE_EMAIL")))
+    return(Sys.getenv("GARGLE_EMAIL"))
+  } else {
+    return("")
+  }
+})
